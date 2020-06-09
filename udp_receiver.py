733ea4dd38f43cb42f65330ae7a2821077b48fab
Aubin Sionville <aubin.sionville@stfrancoislaroche.fr>
2020-06-09 14:28:34 +0200

    Enlevé ce qui n'est pas utile

--- udp_receiver.py
+++ udp_receiver.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 UDP_IP = "192.168.1.40"
 UDP_PORT = 5005
@@ -7,27 +8,14 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
-def hello():
-	print("Quelqu'un est au lab")
-
-def analys(msg):
-	commands = {
-		'hello' : "hello()"
-	}
-
-	try:
-		exec(commands[msg])
-	except KeyError:
-		print("Cette commande n'existe pas")
-	except :
-		print("Erreur inconnue")
-
 def write_command(data):
 	with open('data.txt', 'a') as file:
 		file.write(str(data) + "\n")
 
+time.sleep(10)
 while True:
 	data, addr = sock.recvfrom(1024) # Buffer en 1024
+	print("a")
 	data = str(data)[2:-1]
 	write_command(data)
 
