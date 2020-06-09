9980dfa4846ea91d64715177f1e88591f19174f8
Aubin Sionville <aubin.sionville@stfrancoislaroche.fr>
2020-06-09 14:29:02 +0200

    Ajout gestion subprocess + lecture fichier

--- bot.py
+++ bot.py
@@ -1,36 +1,40 @@
 import discord
 from discord.ext import commands, tasks
+import subprocess
 
 TOKEN = "NjkwNTM3NzIzNzA1OTUwMjM4.XnS31Q.nUBoW61fgZfgo3Zt0P7jrjf7gZU"
 client = commands.Bot(command_prefix = ".")
 
 available_commands = {'hello' : "hello()"}
 
+reception = subprocess.Popen(['python', 'udp_receiver.py'])
+
+async def cleaner():
+    with open('data.txt') as f:
+        print(f.write(""))
+
 @tasks.loop(seconds=30.0)
 async def get_data():
 
     try:
-        with open('data.txt', 'r') as file:
-            current_command = file.readlines()[-1][:-1]
-            try:
-                await exec(available_commands[current_command])
-            except KeyError:
-                print("Cette commande n'existe pas")
-            except Exception as error:
-                print(f"Erreur inconnue : {error}")
-    except:
-        print("Le fichier est déjà ouvert")
+        with open('data.txt') as f:
+            data = f.readlines()
+            print(data)
+            if len(data) >= 1:
+                current_command = data[-1][:-1]
+                try:
+                    await exec(available_commands[current_command])
+                    await cleaner
+                except KeyError:
+                    print("Cette commande n'existe pas")
+                except Exception as error:
+                    print(f"Erreur inconnue : {error}")
+    except Exception as e:
+        print(f"Erreur : {e}")
 
 @client.event
 async def on_ready():
     print('Bot online')
-
-@client.command(pass_context=True)
-async def combien(ctx):
-    channel = ctx.message.author.voice.channel
-    amount = len(channel.members)
-    new_name = str(channel.name) + ' (' + str(amount) + ')'
-    await channel.edit(name=new_name)
 
 get_data.start()
 client.run(TOKEN)
